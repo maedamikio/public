84c6d9b5a61a890dc27bacf9ca49907b8e50c0e3
MAEDA Mikio <maedamikio@users.noreply.github.com>
2019-12-11 00:29:20 +0900

    add image download program

--- abe_or_ishihara/learning/config.py
+++ abe_or_ishihara/learning/config.py
@@ -38,3 +38,4 @@ CLASSES = [
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(BASE_PATH, 'data')
 LINK_PATH = os.path.join(DATA_PATH, 'link')
+DOWNLOAD_PATH = os.path.join(DATA_PATH, 'download')
